a97b529453f13832aef5281c8652a0835bda112b
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-10 13:41:12 +0900

    init

---
+++ src/test/java/com/sparos4th/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparos4th.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
