68172f2e2ae08536530dc698a86e3d92672970ae
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-27 01:56:10 +0900

    feat: 총 기부금 조회api완성(#57)

--- src/main/java/com/sparos4th/admin/admin/application/PaymentService.java
+++ src/main/java/com/sparos4th/admin/admin/application/PaymentService.java
@@ -1,9 +1,12 @@
 package com.sparos4th.admin.admin.application;
 
+import com.sparos4th.admin.admin.domain.payment.TotalDonationSettlement;
 import com.sparos4th.admin.admin.dto.PostDonationResponseDto;
+import com.sparos4th.admin.admin.dto.TotalDonationSettlementResponseDto;
 import java.util.List;
 
 public interface PaymentService {
 
 	List<PostDonationResponseDto> donationList();
+	TotalDonationSettlementResponseDto totalDonation();
 }
