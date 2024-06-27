a69de0aaa022a5fff236a378f67c338823143f8a
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-27 11:23:24 +0900

    feat: 에러문제 해결(#57)

--- src/main/java/com/sparos4th/admin/admin/infrastructure/payment/TotalSettlementRepository.java
+++ src/main/java/com/sparos4th/admin/admin/infrastructure/payment/TotalSettlementRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TotalSettlementRepository extends JpaRepository<TotalDonationSettlement, Long> {
 
-	TotalDonationSettlement findByTotalSettlementId(Long totalSettlementId);
+	Optional<TotalDonationSettlement> findById(Long id);
 }
